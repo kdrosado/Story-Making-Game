393769b46096d894511680698b01053359e17c6a
Kerry D. Rosado <kdrosado@gmail.com>
2016-08-17 22:37:16 -0700

    Created app.js
    Created app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// Story Making Game
+/*
+1. Create a variable and capture input
+2. Create variables to capture each piece of input
+3. Add an alert to tell user they are finished
+4. Combine input with other strings to create message
+5. Print story to document window
+*/
+
+prompt();
+var noun;
+var verb;
+var adjective;
+
+alert("You are finshed");
+
+document.write();
+
